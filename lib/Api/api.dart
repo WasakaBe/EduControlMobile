4385137de077d2cf90bd220693cc8f904b078185
WasakaBe <aureliabelindaalandejesus@gmail.com>
2024-10-13 11:33:08 -0600

    agregacion de api de terceros  "recaptcha text"

--- lib/Api/api.dart
+++ lib/Api/api.dart
@@ -1,4 +1,4 @@
-//const String baseUrl = 'http://localhost:50003/api';
-const String baseUrl = 'https://7488-138-84-54-162.ngrok-free.app/api';
+const String baseUrl = 'http://localhost:50003/api';
+//const String baseUrl = 'https://7488-138-84-54-162.ngrok-free.app/api';
  
  
